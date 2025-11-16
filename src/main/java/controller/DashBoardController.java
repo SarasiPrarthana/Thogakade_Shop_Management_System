45182a60ea4730329533f47752343444999cef10
SarasiPrarthana <prarthanasarasi32@gmail.com>
2025-11-16 21:06:52 +0530

    implement navigation actions in DashBoardController

--- src/main/java/controller/DashBoardController.java
+++ src/main/java/controller/DashBoardController.java
@@ -1,4 +1,56 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class DashBoardController {
+
+    Stage stage = new Stage();
+
+    @FXML
+    void btnCustomerAction(ActionEvent event) {
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/customer_form.fxml"))));
+            stage.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    @FXML
+    void btnItemAction(ActionEvent event) {
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/item_form.fxml"))));
+            stage.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @FXML
+    void btnOrderAction(ActionEvent event) {
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/order_form.fxml"))));
+            stage.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @FXML
+    void btnOrderDetailAction(ActionEvent event) {
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/orderdetail_form.fxml"))));
+            stage.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
---
+++ src/main/java/controller/orderDetail/OrderDetailController.java
@@ -0,0 +1,4 @@
+package controller.orderDetail;
+
+public class OrderDetailController {
+}
