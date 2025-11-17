3fe95cacd48e217551d3cb334590ed592ba6099d
SarasiPrarthana <prarthanasarasi32@gmail.com>
2025-11-17 16:27:37 +0530

    Add CustomerFormController with UI mappings

--- src/main/java/controller/customer/CustomerFormController.java
+++ src/main/java/controller/customer/CustomerFormController.java
@@ -1,4 +1,202 @@
 package controller.customer;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class CustomerFormController {
+
+    @FXML
+    private TableColumn<?, ?> colAddress;
+
+    @FXML
+    private TableColumn<?, ?> colAddress1;
+
+    @FXML
+    private TableColumn<?, ?> colAddress2;
+
+    @FXML
+    private TableColumn<?, ?> colCity;
+
+    @FXML
+    private TableColumn<?, ?> colCity1;
+
+    @FXML
+    private TableColumn<?, ?> colCity2;
+
+    @FXML
+    private TableColumn<?, ?> colDOB;
+
+    @FXML
+    private TableColumn<?, ?> colDOB1;
+
+    @FXML
+    private TableColumn<?, ?> colDOB2;
+
+    @FXML
+    private TableColumn<?, ?> colID;
+
+    @FXML
+    private TableColumn<?, ?> colID1;
+
+    @FXML
+    private TableColumn<?, ?> colID2;
+
+    @FXML
+    private TableColumn<?, ?> colName;
+
+    @FXML
+    private TableColumn<?, ?> colName1;
+
+    @FXML
+    private TableColumn<?, ?> colName2;
+
+    @FXML
+    private TableColumn<?, ?> colPostalCode;
+
+    @FXML
+    private TableColumn<?, ?> colPostalCode1;
+
+    @FXML
+    private TableColumn<?, ?> colPostalCode2;
+
+    @FXML
+    private TableColumn<?, ?> colProvince;
+
+    @FXML
+    private TableColumn<?, ?> colProvince1;
+
+    @FXML
+    private TableColumn<?, ?> colProvince2;
+
+    @FXML
+    private TableColumn<?, ?> colSalary;
+
+    @FXML
+    private TableColumn<?, ?> colSalary1;
+
+    @FXML
+    private TableColumn<?, ?> colSalary2;
+
+    @FXML
+    private TableColumn<?, ?> colTitle;
+
+    @FXML
+    private TableColumn<?, ?> colTitle1;
+
+    @FXML
+    private TableColumn<?, ?> colTitle2;
+
+    @FXML
+    private TextField txtAddress;
+
+    @FXML
+    private TextField txtAddress1;
+
+    @FXML
+    private TextField txtAddress2;
+
+    @FXML
+    private TextField txtCity;
+
+    @FXML
+    private TextField txtCity1;
+
+    @FXML
+    private TextField txtCity2;
+
+    @FXML
+    private TextField txtCustID;
+
+    @FXML
+    private TextField txtCustID1;
+
+    @FXML
+    private TextField txtCustID2;
+
+    @FXML
+    private TextField txtDOB;
+
+    @FXML
+    private TextField txtDOB1;
+
+    @FXML
+    private TextField txtDOB2;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private TextField txtName1;
+
+    @FXML
+    private TextField txtName2;
+
+    @FXML
+    private TextField txtPostalCode;
+
+    @FXML
+    private TextField txtPostalCode1;
+
+    @FXML
+    private TextField txtPostalCode2;
+
+    @FXML
+    private TextField txtProvince;
+
+    @FXML
+    private TextField txtProvince1;
+
+    @FXML
+    private TextField txtProvince2;
+
+    @FXML
+    private TextField txtSalary;
+
+    @FXML
+    private TextField txtSalary1;
+
+    @FXML
+    private TextField txtSalary2;
+
+    @FXML
+    private TableView<?> txtTbl;
+
+    @FXML
+    private TableView<?> txtTbl1;
+
+    @FXML
+    private TableView<?> txtTbl2;
+
+    @FXML
+    private TextField txtTitle;
+
+    @FXML
+    private TextField txtTitle1;
+
+    @FXML
+    private TextField txtTitle2;
+
+    @FXML
+    void btnAddAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnClearAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDeleteAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUpdateAction(ActionEvent event) {
+
+    }
+
 }
